5f7ed19f7b2e0e565f4526bdab609706eb1323e1
Your Name <you@example.com>
2019-09-10 20:39:19 -0300

    Anuncio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AnunciarComponent } from './anunciar/anunciar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AnunciarComponent
   ],
   imports: [
     BrowserModule
