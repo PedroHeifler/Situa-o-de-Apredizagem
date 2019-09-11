c905c708b9b27618d2cb01ebc4046e1c0c32327d
Your Name <you@example.com>
2019-09-10 21:01:30 -0300

    Teste
    Teste miguelito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.component';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { CarinhoComponent } from './carinho/carinho.component';
+import { DetalheProdutoComponent } from './detalhe-produto/detalhe-produto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CadastroPessoaComponent,
-    HomeComponentComponent
+    HomeComponentComponent,
+    CarinhoComponent,
+    DetalheProdutoComponent
   ],
   imports: [
     BrowserModule
