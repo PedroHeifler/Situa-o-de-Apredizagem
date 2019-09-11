75e907dcbd381e53a477573efe09419e065f8944
Your Name <you@example.com>
2019-09-11 19:36:15 -0300

    Correcao

---
+++ src/app/anunciar/anunciar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anunciar',
+  templateUrl: './anunciar.component.html',
+  styleUrls: ['./anunciar.component.css']
+})
+export class AnunciarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AnunciarComponent } from './anunciar/anunciar.component';
+import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.component';
+import { HomeComponentComponent } from './home-component/home-component.component';
+import { CarinhoComponent } from './carinho/carinho.component';
+import { DetalheProdutoComponent } from './detalhe-produto/detalhe-produto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    AnunciarComponent,
+    CadastroPessoaComponent,
+    HomeComponentComponent,
+    CarinhoComponent,
+    DetalheProdutoComponent
   ],
   imports: [
     BrowserModule,
