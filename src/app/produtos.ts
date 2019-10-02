014ae5ac1ce322797a3be5486471da46c9e0c7b8
Your Name <you@example.com>
2019-10-02 20:23:37 -0300

    service do detalhes com produtos.ts

--- src/app/produtos.ts
+++ src/app/produtos.ts
@@ -1,16 +1,74 @@
+// export const produtos = [
+//     {
+//       nome: '',
+//       preco_unit: '',
+//       quantidade:'',
+//       tipo: '',
+//       moeda: '',
+//       entrega: '',
+//       descricao: '',
+//       cidade: '',
+//       estado: '',
+//       pais: '',
+//       cep: '',
+//       imagem_produto: ''
+//     },
+//   ];
 export const produtos = [
-    {
-      nome: '',
-      preco_unit: '',
-      quantidade:'',
-      tipo: '',
-      moeda: '',
-      entrega: '',
-      descricao: '',
-      cidade: '',
-      estado: '',
-      pais: '',
-      cep: '',
-      imagem_produto: ''
-    },
-  ];
+  {
+    
+    nome: 'Munitor Top',
+    valor: '600',
+    descricao: 'Melhor munitor do mundo 8k ultra HD, mais de 1bilhao de cores',
+    imagem: 'monitor.jpg'
+  },
+  {
+    
+    nome: 'Mouse Gamer',
+    valor: '60',
+    descricao: 'Mouse Gamer RGB com 5000dpi e durabilidade de 10 milhoes de cliques',
+    imagem: 'mouse.jpg'
+  },
+  {
+    
+    nome: 'Teclado Top',
+    valor: '300',
+    descricao: 'Teclado Mecanico com RGB, Melhor preco do Brasil',
+    imagem: 'teclado.jpg'
+  },
+  {
+    
+    nome: 'Gabinete Top',
+    valor: '400',
+    descricao: 'Gabinete Transparente com cooler RGB Hiper Ventilacao',
+    imagem: 'gabinete-gamer-c3-tech-mt-g1000bk-preto.jpg'
+  },
+  {
+    
+    nome: 'Munitor Top',
+    valor: '600',
+    descricao: 'Melhor munitor do mundo 8k ultra HD, mais de 1bilhao de cores',
+    imagem: 'monitor.jpg'
+  },
+  {
+    
+    nome: 'Mouse Gamer',
+    valor: '60',
+    descricao: 'Mouse Gamer RGB com 5000dpi e durabilidade de 10 milhoes de cliques',
+    imagem: 'mouse.jpg'
+  },
+  {
+    
+    nome: 'Teclado Top',
+    valor: '300',
+    descricao: 'Teclado Mecanico com RGB, Melhor preco do Brasil',
+    imagem: 'teclado.jpg'
+  },
+  {
+    
+    nome: 'Gabinete Top',
+    valor: '400',
+    descricao: 'Gabinete Transparente com cooler RGB Hiper Ventilacao',
+    imagem: 'gabinete-gamer-c3-tech-mt-g1000bk-preto.jpg'
+  },
+]
