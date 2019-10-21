292b5dfe4e1f7676dd238a6a344c9ac7e98296e6
Your Name <you@example.com>
2019-10-21 20:13:57 -0300

    Arrumando endereco

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule,FormsModule } from '@angular/forms'
-import { EnderecoComponent } from './endereco/endereco.component';
 import { AppComponent } from './app.component';
 import { CadastroPessoaComponent } from './cadastro-pessoa/cadastro-pessoa.component';
 import { HomeComponent } from './home/home.component';
@@ -10,7 +9,6 @@ import { LoginComponent } from './login/login.component';
 import { AnunciarComponent } from './anunciar/anunciar.component';
 import { DetalheProdutoComponent } from './detalhe-produto/detalhe-produto.component';
 import { AppRoutingModule } from './app-routing.module';
-
 import { AnunciarService } from './anunciar/anunciar.service';
 import { EnderecoComponent } from './endereco/endereco.component';
 import { CadastroPessoaService } from './cadastro-pessoa/cadastro-pessoa.service';
