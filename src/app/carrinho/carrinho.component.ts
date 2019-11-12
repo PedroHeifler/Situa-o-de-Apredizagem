f33fa841bd1e3dee075031f1ee2850231c1649c2
Murakami <mauriciomurakami@hotmail.com>
2019-11-11 22:42:53 -0200

    Frete do carrinho

--- src/app/carrinho/carrinho.component.ts
+++ src/app/carrinho/carrinho.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+import * as Inputmask from "inputmask";
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-Carrinho',
@@ -10,6 +13,12 @@ export class CarrinhoComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    Inputmask().mask(document.querySelectorAll("input"));
   }
 
+  OnClickCalcularFrete() {
+    let random = Math.floor((Math.random() * 40) + 10);
+    
+    $("#frete").text("Frete: R$ " + random.toFixed(2));
+  }
 }
